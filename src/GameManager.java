fb24b908c660004efa5b227170a7a919fb96a406
AanRlis <ancns1325@hanyang.ac.kr>
2025-12-02 16:17:02 +0900

    :star: Updated GameManager & :wrench: Added scratch library setting

--- src/GameManager.java
+++ src/GameManager.java
@@ -2,7 +2,6 @@ import org.openpatch.scratch.KeyCode;
 import org.openpatch.scratch.RotationStyle;
 import org.openpatch.scratch.Sprite;
 import org.openpatch.scratch.Stage;
-import org.openpatch.scratch.extensions.math.Vector2;
 
 public class GameManager extends Stage {
 
@@ -12,9 +11,9 @@ public class GameManager extends Stage {
     double gameTimer = 480.0;
     static boolean isGamePaused = false;
     private int waveCounter = 0;
-    private int[] snails = {10, 10, 15, 15, 10, 10};
-    private int[] slimes = {0, 3, 0, 3, 5, 5};
-    private int[] boars = {0, 0, 0, 0, 0, 2};
+    private final int[] snails = {10, 10, 15, 15, 10, 10};
+    private final int[] slimes = {0, 3, 0, 3, 5, 5};
+    private final int[] boars = {0, 0, 0, 0, 0, 2};
 
     public GameManager() {
         super(800, 600);
@@ -30,11 +29,13 @@ public class GameManager extends Stage {
             this.add(t);
         }
 
+        spawnWave();
     }
 
     public static void main(String[] args) {
         new GameManager();
     }
+
     @Override
     public void run() {
         //Stop on Pause
@@ -43,19 +44,22 @@ public class GameManager extends Stage {
         waveTimer++;
 
         gameTimer -= (1.0 / 60.0);
+
         // 0초 이하로 내려가지 않게 고정
         if (gameTimer < 0) gameTimer = 0;
 
-
-        if (waveTimer >= 900 || isFirstWave) {
-            spawnWave();   // 몬스터 소환
-            waveTimer = 0; // 0으로 초기화 (다시 15초 세기 시작)
+        if (isFirstWave || waveTimer > 900) {
             isFirstWave = false;
+            waveTimer = 0;
+            spawnWave();
         }
-
     }
+
+
     private void spawnWave() {
-        if waveCounter > snails.length { waveCounter = 0; }
+        if (waveCounter >= snails.length) {
+            waveCounter = 0;
+        }
 
         for (int i = 0; i < snails[waveCounter]; i++) {
             this.add(new Enemy(Enemy.snail));
@@ -66,7 +70,10 @@ public class GameManager extends Stage {
         for (int i = 0; i < boars[waveCounter]; i++) {
             this.add(new Enemy(Enemy.boar));
         }
+
+        waveCounter++;
     }
+
     public static void AddCostumes(Sprite spr, String path, String name, int range){
         for (int i = 1; i <= range; i++) {
             spr.addCostume(name + i, path + name + i + ".png");
